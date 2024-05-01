5fb8253b95cf60b1b2328a3d3bb4557384c0a5af
Ravi Shankar Mishra <150536398+imravismishra@users.noreply.github.com>
2024-05-01 10:38:52 -0400

    Add files via upload

---
+++ wishlist-service/src/main/java/com/cogent/main/dto/CategoryDao.java
@@ -0,0 +1,17 @@
+package com.cogent.main.dto;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDao {
+
+	private String name;
+}
